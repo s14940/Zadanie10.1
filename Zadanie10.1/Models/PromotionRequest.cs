54eefea9ff8b4db3a7f7d89f547c0176b7f8bd6b
Michał Wróblewski <michal.wroblewski@plus.pl>
2020-05-21 13:53:48 +0200

    Zadanie10.1

---
+++ Zadanie10.1/Models/PromotionRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zadanie10._1.Models
+{
+    public class PromotionRequest
+    {
+        public String Studies { get; set; }
+        public int Semester { get; set; }
+    }
+}
